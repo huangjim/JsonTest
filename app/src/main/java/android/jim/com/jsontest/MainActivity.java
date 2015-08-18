ef64e6ecb8ee2375fa8ef56cc3cb700ca35ff542
jim huang <617963905@qq.com>
2015-08-18 21:01:10 +0800

    success

--- app/src/main/java/android/jim/com/jsontest/MainActivity.java
+++ app/src/main/java/android/jim/com/jsontest/MainActivity.java
@@ -1,64 +1,90 @@
 package android.jim.com.jsontest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
 
-import org.json.JSONObject;
-
-import java.lang.reflect.Method;
-import java.util.Map;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
     private RequestQueue queue;
     private Button button;
+    private Gson gson;
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        queue= Volley.newRequestQueue(this);
-        textView=(TextView)findViewById(R.id.content_text);
-        button=(Button)findViewById(R.id.button);
+        //Volley实例化
+        queue = Volley.newRequestQueue(this);
+        textView = (TextView) findViewById(R.id.content_text);
+        button = (Button) findViewById(R.id.button);
 
-        //StringRequest
-       /* StringRequest stringRequest=new StringRequest(Request.Method.POST,"http://qq.com",
+        gson = new Gson();
+
+        try {
+            //这个URL是自己在网上申请的一个天气接口
+            String addressCode = URLEncoder.encode("北京", "UTF-8");
+            url = "http://apis.haoservice.com/weather?cityname=" + addressCode + "&key=b2827fdb1ceb477684fdd442b0100fa9";
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        //Volley的StringRequest方法
+        final StringRequest stringRequest = new StringRequest(url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String s) {
-
-                        Log.d("TAG",s);
-                        textView.setText(s);
+                        if (!TextUtils.isEmpty(s)) {
+                            Log.d("TAG", s);
+                            System.out.println(s);
+                            Status status = gson.fromJson(s, Status.class);
+                            MyResult myResult = status.getResult();
+                            MyToday myToday = myResult.getToday();
+                            //这是我想要得到的数据城市+日期+温度+天气情况+穿衣建议
+                            String msg = myToday.getCity() + myToday.getDate_y() + myToday.getTemperature() + myToday.getWeather() + "\n" + myToday.getDressing_advice();
+                            textView.setText(msg);
+                        } else {
+                            Toast.makeText(MainActivity.this, "加载失败", Toast.LENGTH_SHORT).show();
+                        }
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError volleyError) {
-                Log.e("TAG",volleyError.getMessage(),volleyError);
+                Log.e("TAG", volleyError.getMessage(), volleyError);
             }
-        }){
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                return super.getParams();
-            }
-        };
-        queue.add(stringRequest);*/
+        });
 
-        //JSONRequest
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //点击触发下载
+                queue.add(stringRequest);
+
+            }
+        });
+
+    }
+
+}
+
+//JSONRequest
         /*JsonObjectRequest jsonObjectRequest=new JsonObjectRequest("http://apis.haoservice.com/weather?cityname=北京&key=b2827fdb1ceb477684fdd442b0100fa9",
                 null, new Response.Listener<JSONObject>() {
             @Override
@@ -76,14 +102,3 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
         queue.add(jsonObjectRequest);*/
-
-    }
-    class MyThread extends Thread{
-        @Override
-        public void run() {
-            super.run();
-        }
-    }
-
-
-}
--- app/src/main/java/android/jim/com/jsontest/Status.java
+++ app/src/main/java/android/jim/com/jsontest/Status.java
@@ -4,15 +4,15 @@ package android.jim.com.jsontest;
  * Created by huangjim on 8/18/2015.
  */
 public class Status {
-    private String error_code;
+    private Integer error_code;
     private String reason;
     private MyResult result;
 
-    public String getError_code() {
+    public Integer getError_code() {
         return error_code;
     }
 
-    public void setError_code(String error_code) {
+    public void setError_code(Integer error_code) {
         this.error_code = error_code;
     }
 
